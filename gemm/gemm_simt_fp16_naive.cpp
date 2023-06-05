89f3a786b4fba4118e87a9821330caa62e8495cd
Yutao Xu <yutao.xu@intel.com>
2023-06-05 18:05:21 +0800

    Create gemm_simt_fp16_naive.cpp

---
+++ gemm/gemm_simt_fp16_naive.cpp
@@ -0,0 +1,154 @@
+#include <CL/sycl.hpp>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <chrono>
+
+double timeit(cl::sycl::event &event) {
+    auto submit_time = event.get_profiling_info<cl::sycl::info::event_profiling::command_submit>();
+    auto start_time = event.get_profiling_info<cl::sycl::info::event_profiling::command_start>();
+    auto end_time = event.get_profiling_info<cl::sycl::info::event_profiling::command_end>();
+    auto submission_time = (start_time - submit_time) / 1000000.0f;
+    auto execution_time = (end_time - start_time) / 1000000.0f;
+    return execution_time;
+}
+
+template <typename scalar_t>
+void gemm_cpu(
+    scalar_t *out,
+    const scalar_t *a,
+    const scalar_t *b,
+    const int m, const int n, const int k,
+    const scalar_t alpha,
+    const scalar_t beta) {
+    for (int mi = 0; mi < m; mi++) {
+        for (int ni = 0; ni < n; ni++) {
+            float acc = 0;
+            for (int ki = 0; ki < k; ki++) {
+                acc += (float)a[mi * k + ki] * (float)b[ki * n + ni];
+            }
+            out[mi * n + ni] = (float)alpha * acc + (float)beta * (float)out[mi * n + ni];
+        }
+    }
+}
+
+template <typename scalar_t, typename item_t>
+inline void gemm_xpu_kernel(
+    item_t &item,
+    scalar_t *out,
+    const scalar_t *a,
+    const scalar_t *b,
+    const int m, const int n, const int k,
+    const scalar_t alpha,
+    const scalar_t beta) {
+    auto mi = item.get_group(0) * 32 + item.get_local_id(0); // y
+    auto ni = item.get_group(1) * 32 + item.get_local_id(1);
+    if (mi < m && ni < n) {
+        float acc = 0.f;
+        for (int ki = 0; ki < k; ki++) {
+            acc += (float)a[mi * k + ki] * (float)b[ki * n + ni];
+        }
+        auto r = (float)alpha * acc;
+        out[mi * n + ni] = r + (float)beta * (float)out[mi * n + ni];
+    }
+}
+
+template <typename scalar_t>
+float gemm_xpu(
+    sycl::queue &q,
+    scalar_t *out,
+    const scalar_t *a,
+    const scalar_t *b,
+    const int m, const int n, const int k,
+    const scalar_t alpha,
+    const scalar_t beta) {
+    auto m_groups = (m + 32 - 1) / 32; // y
+    auto n_groups = (n + 32 - 1) / 32;
+    auto event = q.submit([&](sycl::handler& h){
+        h.parallel_for(
+            sycl::nd_range<2>(
+                sycl::range<2>(m_groups * 32, n_groups * 32), 
+                sycl::range<2>(32, 32)),
+            [=](sycl::nd_item<2> item) {
+                gemm_xpu_kernel<scalar_t>(item, out, a, b, m, n, k, alpha, beta);
+        });
+    });
+    event.wait();
+    return timeit(event);
+}
+
+struct gemm_sizes {
+    int m, n, k;
+    float alpha, beta;
+    gemm_sizes(int m_, int n_, int k_, float a, float b) :
+        m(m_), n(n_), k(k_), alpha(a), beta(b) {
+    }
+};
+
+int main() {
+    std::cout << "gemm_simt_fp16_naive\n";
+    sycl::queue queue(sycl::gpu_selector_v, cl::sycl::property_list {cl::sycl::property::queue::enable_profiling()});
+    using scalar_t = sycl::half;
+
+    std::vector<gemm_sizes> sizes;
+    sizes.push_back(gemm_sizes(64, 64, 64, 0.5, 0.5));
+    sizes.push_back(gemm_sizes(1024, 1024, 1024, 0.5, 0.5));
+    // sizes.push_back(gemm_sizes(2048, 2048, 2048, 0.5, 0.5));
+    
+    for (auto size : sizes) {
+        int m = size.m;
+        int n = size.n;
+        int k = size.k;
+        auto alpha = size.alpha;
+        auto beta = size.beta;
+
+        std::cout << "m=" << m << ", n=" << n << ", k=" << k
+                  << ", alpha=" << alpha << ", beta=" << beta << "\n";
+
+        auto a_cpu = new scalar_t[m * k];
+        auto b_cpu = new scalar_t[k * n];
+        auto out_cpu = new scalar_t[m * n];
+        for (int i = 0; i < m * k; i++)
+            a_cpu[i] = static_cast<scalar_t>((float)rand() / (float)RAND_MAX);
+        for (int i = 0; i < k * n; i++)
+            b_cpu[i] = static_cast<scalar_t>((float)rand() / (float)RAND_MAX);
+        for (int i = 0; i < m * n; i++)
+            out_cpu[i] = static_cast<scalar_t>((float)rand() / (float)RAND_MAX);
+
+        auto a_xpu = sycl::aligned_alloc_device<scalar_t>(256, m * k, queue);
+        auto b_xpu = sycl::aligned_alloc_device<scalar_t>(256, k * n, queue);
+        auto out_xpu = sycl::aligned_alloc_device<scalar_t>(256, m * n, queue);
+        queue.memcpy(a_xpu, a_cpu,  m * k * sizeof(scalar_t)).wait();
+        queue.memcpy(b_xpu, b_cpu,  k * n * sizeof(scalar_t)).wait();
+        queue.memcpy(out_xpu, out_cpu,  m * n * sizeof(scalar_t)).wait();
+
+        gemm_cpu<scalar_t>(out_cpu, a_cpu, b_cpu, m, n, k, alpha, beta);
+        auto timems = gemm_xpu<scalar_t>(queue, out_xpu, a_xpu, b_xpu, m, n, k, alpha, beta);
+
+        double total_gbytes = ((double)m * k + k * n + m * n + m * n) * sizeof(scalar_t) / 1000.0 / 1000 / 1000;
+        std::cout << timems << " ms, " << total_gbytes / (timems / 1000.0) << " gbps, ";
+
+        double tflops = ((double)2 * m * n * k) / (timems / 1000) * 1e-12;
+        std::cout << tflops << " tflops\n";
+
+        auto out_xpu_ = new scalar_t[m * n];
+        queue.memcpy(out_xpu_, out_xpu,  m * n * sizeof(scalar_t)).wait();
+        auto maxdiff = -std::numeric_limits<float>::infinity();
+        for (int i = 0; i < m * n; i++) {
+            // if (i < 10)
+            //     std::cout << (float)out_xpu_[i] << " " << (float)out_cpu[i] << "\n";
+            auto diff = std::abs((float)out_xpu_[i] - (float)out_cpu[i]);
+            maxdiff = std::max(maxdiff, diff);
+        }
+        std::cout << "maxdiff: " << maxdiff << std::endl;
+
+        sycl::free(a_xpu, queue);
+        sycl::free(b_xpu, queue);
+        sycl::free(out_xpu, queue);
+        delete[] a_cpu;
+        delete[] b_cpu;
+        delete[] out_cpu;
+        delete[] out_xpu_;
+    }
+    return 0;
+}
