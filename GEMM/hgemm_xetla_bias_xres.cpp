27c536a233702b8434da075e40c96d959bc1ecaa
Yutao Xu <yutao.xu@intel.com>
2023-07-04 15:35:15 +0800

    Update hgemm_xetla_bias_xres.cpp

--- GEMM/hgemm_xetla_bias_xres.cpp
+++ GEMM/hgemm_xetla_bias_xres.cpp
@@ -26,6 +26,80 @@ double timeit(cl::sycl::event &event) {
     return execution_time;
 }
 
+template <typename dtype_in_>
+struct xres_op_t {
+    using dtype_in = dtype_in_;
+    using mem_desc_in_t = mem_desc_t<dtype_in, mem_layout::row_major, mem_space::global>;
+    using shape_t = typename mem_desc_in_t::shape_t;
+    using coord_t = typename mem_desc_in_t::coord_t;
+    using base_t = typename mem_desc_in_t::base_t;
+
+    struct arguments_t {
+        shape_t shape;
+        base_t base;
+        dtype_in x;
+        inline arguments_t() = default;
+        inline arguments_t(base_t base_, shape_t shape_, dtype_in x_) :
+            base(base_), shape(shape_), x(x_) {
+        }
+    };
+    template <typename matAcc_t>
+    __XETLA_API KERNEL_FUNC void operator()(matAcc_t &matAcc,
+                                            const coord_t &coord, const arguments_t &args,
+                                            uint32_t slm_base = 0, uint32_t nbarrier_base = 0) {
+        using dtype_acc = typename matAcc_t::dtype;
+        static constexpr uint32_t tile_size_x = matAcc_t::tile_size_x;
+        static constexpr uint32_t tile_size_y = matAcc_t::tile_size_y;
+        static constexpr uint32_t block_size_x = matAcc_t::block_size_x;
+        static constexpr uint32_t block_size_y = matAcc_t::block_size_y;
+        static constexpr int32_t num_block_x = matAcc_t::num_block_x;
+        static constexpr int32_t num_block_y = matAcc_t::num_block_y;
+        static constexpr uint32_t tile_elems = matAcc_t::tile_elems;
+        static constexpr uint32_t block_elems = matAcc_t::block_elems;
+
+        using mat_in_tile_desc_t = tile_desc_t<tile_size_x, tile_size_y,
+                                               block_size_x, block_size_y, reg_layout::tiled>;
+        using mat_in_tile_t = tile_t<dtype_in, mat_in_tile_desc_t>;
+        using mat_in_payload_t = mem_payload_t<dtype_in, mat_in_tile_desc_t,
+                                               msg_type_v<mat_in_tile_desc_t, mem_desc_in_t::space>,
+                                               mem_desc_in_t::layout, mem_desc_in_t::space, gpu_arch::Xe>;
+        using mat_in_tile_acc_t = tile_t<dtype_acc, mat_in_tile_desc_t>;
+        mem_desc_in_t mem_desc_in(args.base, args.shape, coord);
+        mat_in_tile_t mat_in;
+        mat_in_payload_t mat_in_payload(mem_desc_in);
+        tile_load<cache_hint::cached, cache_hint::cached>(
+            mat_in, mat_in_payload);
+        mat_in_tile_acc_t mat_in_acc;
+        elemwise_cvt(mat_in_acc, mat_in);
+
+#pragma unroll
+        for (int i = 0; i < tile_size_y / block_size_y; i++) {
+#pragma unroll
+            for (int j = 0; j < num_block_x; j++) {
+                auto dst_reg = matAcc.reg.xetla_select<block_elems, 1>(
+                    (i * num_block_x + j) * block_elems);
+                auto src_reg = mat_in_acc.reg.xetla_select<block_elems, 1>(
+                    (i * num_block_x + j) * block_elems);
+                dst_reg = dst_reg + args.x * src_reg;
+            }
+        }
+        // process the tail
+        if constexpr ((tile_size_y % block_size_y) != 0) {
+            constexpr uint32_t tail_start_y = tile_size_y / block_size_y * block_size_y;
+            constexpr int32_t tail_size_y = tile_size_y % block_size_y;
+            constexpr int32_t tail_block_elems = tail_size_y * block_size_x;
+#pragma unroll
+            for (int j = 0; j < num_block_x; j++) {
+                auto dst_reg = matAcc.reg.xetla_select<tail_block_elems, 1>(
+                    tail_start_y * tile_size_x + j * tail_block_elems);
+                auto src_reg = mat_in_acc.reg.xetla_select<tail_block_elems, 1>(
+                    tail_start_y * tile_size_x + j * tail_block_elems);
+                dst_reg = dst_reg + args.x * src_reg;
+            }
+        }
+    }
+};
+
 template <
     typename scalar_t,
     int WG_M = 8,
@@ -112,10 +186,7 @@ float gemm_xpu(
                 epilogue_policy_tile_op<
                     chained_tile_op_t<
                         bias_add_op_t<data_type_bias, gpu_arch::Xe>,
-                        elemwise_reduce_op_t<
-                            reduce_op::sum,
-                            data_type_res,
-                            gpu_arch::Xe>>,
+                        xres_op_t<data_type_res>>,
                     update_method,
                     gpu_arch::Xe>,
                 tile_shape,
@@ -136,7 +207,7 @@ float gemm_xpu(
                 out,
                 ldc,
                 {{{const_cast<scalar_t *>(bias), {matrix_n, 1, matrix_n}},
-                  {const_cast<scalar_t *>(res), {matrix_n, matrix_m, matrix_n}}}});
+                  {const_cast<scalar_t *>(res), {matrix_n, matrix_m, matrix_n}, x}}});
             slm_barrier_init<gemm_op_t>();
             gemm_op_t gemm_op;
             gemm_op(ei, arg);
