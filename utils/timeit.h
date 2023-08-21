59a99bd594880e0a2e4774c95d1dd0cbf7fcdf5a
Yutao Xu <yutao.xu@intel.com>
2023-08-21 14:47:00 +0800

    Create timeit.h

---
+++ utils/timeit.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <CL/sycl.hpp>
+#include <iostream>
+
+double timeit(cl::sycl::event &event) {
+    auto submit_time = event.get_profiling_info<cl::sycl::info::event_profiling::command_submit>();
+    auto start_time = event.get_profiling_info<cl::sycl::info::event_profiling::command_start>();
+    auto end_time = event.get_profiling_info<cl::sycl::info::event_profiling::command_end>();
+    auto submission_time = (start_time - submit_time) / 1000000.0f;
+    auto execution_time = (end_time - start_time) / 1000000.0f;
+    return execution_time;
+}
