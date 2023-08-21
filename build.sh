e82a3036b117b41249e27dc561838f3c5e04f6d5
Yutao Xu <yutao.xu@intel.com>
2023-08-21 14:37:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ export ZE_AFFINITY_MASK=0.0
 
 xetla_base=/home/gta/xyt/xetla/
 icpx -fsycl -std=c++20 \
+  -Iutils \
   -DXETPP_NEW_XMAIN  \
   -isystem ${xetla_base} \
   -isystem ${xetla_base}/include \
